8c7ebb1aa1a8eeb1e03bcb7572344a0ce068c59d
Lexcaliburr <lishiqi0111@gmail.com>
2022-05-15 16:12:17 +0800

    add: combine shared libs and combine static libs

---
+++ cmake/combin_static_libs/src/ccc.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Author: lishiqi lishiqi@lishiqi.con
+ * @Date: 2022-05-09 01:30:42
+ * @LastEditors: lishiqi lishiqi@lishiqi.con
+ * @LastEditTime: 2022-05-09 02:10:23
+ * @FilePath: /cpp/动态库连接是否会传递/src/aaa.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+#include "bbb.h"
+#include "ccc.h"
+
+C::C()
+{
+
+    B b;
+    std::cout << "construt C" << std::endl;
+
+}
+
+C::~C()
+{
+    std::cout << "324213424 " << std::endl;
+}
---
+++ cmake/combin_static_libs/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Author: lishiqi lishiqi@lishiqi.con
+ * @Date: 2022-05-09 01:36:02
+ * @LastEditors: lishiqi lishiqi@lishiqi.con
+ * @LastEditTime: 2022-05-09 02:10:02
+ * @FilePath: /cpp/动态库连接是否会传递/src/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ *
+ */
+#include <iostream>
+#include "ccc.h"
+
+int main()
+{
+    C c;
+
+    std::cout << "finished " << std::endl;
+};
